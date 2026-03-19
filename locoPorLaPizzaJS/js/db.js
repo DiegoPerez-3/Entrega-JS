0b0bd242c22625fe84d5f1c5cfd677456ba86b2c
Diego Perez <diegojpm2001@gmail.com>
2026-03-19 20:35:24 -0300

    Primer commit

---
+++ locoPorLaPizzaJS/js/db.js
@@ -0,0 +1,42 @@
+let productos = [
+    {
+        id: 1,
+        nombre: "Pizza de Muzzarella",
+        precio: 6500
+    },
+    {
+        id: 2,
+        nombre: "Jamón",
+        precio: 6500
+    },
+    {
+        id: 3,
+        nombre: "Jamón y Morron",
+        precio: 6500
+    },
+    {
+        id:4,
+        nombre: "Napolitana",
+        precio: 8000
+    },
+    {
+        id: 5,
+        nombre: "Primavera",
+        precio: 8500
+    },
+    {
+        id: 6,
+        nombre: "Calabresa",
+        precio: 9500
+    },
+    {
+        id: 7,
+        nombre: "Palmito",
+        precio:7500
+    },
+    {
+        id: 8,
+        nombre: "Fugazzeta",
+        precio: 6500
+    }
+]
